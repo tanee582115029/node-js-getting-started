8a3fc2a66e00b5c02958c44ae7aa2a96a464e32c
Tanee <tanee_yooyen@cmu.ac.th>
2020-12-10 13:59:54 +0700

    Add cool face API

--- index.js
+++ index.js
@@ -14,7 +14,22 @@ app.post('/webhook', function (req,res) {
     'Content-Type': 'application/json',
     'Authorization': `Bearer {hYfoRe8zpVdWHXoXkYCpln3fplbY8uFWOc94nQEmfIvCKWsp7jNKzDKFQSbK2ImrnCtLEYyATbRlzJhWmXlrQgZc0ospynOPobSs4j8Aq7AE3rsTmTJn+0DuDe5dvsxWbrw9oFlSTZixccwZ/hXFVAdB04t89/1O/w1cDnyilFU=}`,
   }
+  const replyToken = req.body.events[0].replyToken
+  const userId = req.body.events[0].source.userId
+  const body = JSON.stringify({
+    replyToken: replyToken,
+    messages: [
+      {
+        type: 'text',
+        text: 'hi'
+      }
+    ],
+  })
+  fetch('https://api.line.me/v2/bot/message/reply', {
+      method: 'POST',
+      headers,
+      body,
+  })
+  res.sendStatus(200)
 
-
-  res.json('hi',200)
 })
