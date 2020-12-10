2581f8d384deea8c7ec991535a3a7130d0a26594
Tanee <tanee_yooyen@cmu.ac.th>
2020-12-10 13:58:45 +0700

    Add cool face API

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.listen(3000, function () { console.log('run on 3000') })
+app.listen(process.env.PORT || 3000, function () { console.log('run on 3000') })
 
 app.post('/webhook', function (req,res) {
+
+  const headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer {hYfoRe8zpVdWHXoXkYCpln3fplbY8uFWOc94nQEmfIvCKWsp7jNKzDKFQSbK2ImrnCtLEYyATbRlzJhWmXlrQgZc0ospynOPobSs4j8Aq7AE3rsTmTJn+0DuDe5dvsxWbrw9oFlSTZixccwZ/hXFVAdB04t89/1O/w1cDnyilFU=}`,
+  }
+
+
   res.json('hi',200)
 })
